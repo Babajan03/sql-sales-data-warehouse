e16aeb37d3fd53a5188bac45ab4b0e951d15289a
Shaik Babajan <125242543+Babajan03@users.noreply.github.com>
2026-02-25 16:32:28 +0530

    Delete etl/load_data.sql

--- etl/load_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO DimCustomer (CustomerID, CustomerName, City)
-SELECT CustomerID, CustomerName, City
-FROM Customers;
-
-INSERT INTO DimProduct (ProductID, ProductName, Category)
-SELECT ProductID, ProductName, Category
-FROM Products;
