566b026e652d9497a7a8d1ef68cbc0bf58b9eaf8
Shaik Babajan <125242543+Babajan03@users.noreply.github.com>
2026-02-25 16:35:40 +0530

    Add indexes for FactSales table

--- indexing/create_indexes.sql
+++ indexing/create_indexes.sql
@@ -1,1 +1,4 @@
 
+CREATE INDEX idx_FactSales_CustomerKey ON FactSales(CustomerKey);
+CREATE INDEX idx_FactSales_ProductKey ON FactSales(ProductKey);
+CREATE INDEX idx_FactSales_OrderDate ON FactSales(OrderDate);
